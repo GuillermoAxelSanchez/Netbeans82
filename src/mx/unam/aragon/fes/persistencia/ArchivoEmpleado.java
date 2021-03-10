9f45e6a4dbb1aea2468f48f9622dbe337efcd0be
GuillermoAxelSanchezOjeda <guillermoaxelsanchez@gmail.com>
2021-03-10 17:16:01 -0600

    10/3/21 conexión a disco duro

--- src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
+++ src/mx/unam/aragon/fes/persistencia/ArchivoEmpleado.java
@@ -1,10 +1,14 @@
 package mx.unam.aragon.fes.persistencia;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.ArrayList;
 import mx.unam.aragon.fes.Empleado;
 
-
 public class ArchivoEmpleado {
+
     private String archivo;
 
     public ArchivoEmpleado() {
@@ -21,17 +25,39 @@ public class ArchivoEmpleado {
     public void setArchivo(String archivo) {
         this.archivo = archivo;
     }
-    
-    public void guardarEmpleados(ArrayList<Empleado> datos){
+
+    public void guardarEmpleados(ArrayList<Empleado> datos) {
         //aquí el código para grabar en disco duro
+        try {
+            ObjectOutputStream fSalida = new ObjectOutputStream(
+                    new FileOutputStream(archivo));
+
+            fSalida.writeObject(datos);
+            fSalida.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
-    
-    public ArrayList<Empleado> leerEmpleados(){
-        ArrayList<Empleado> tmp=null;
-        
+
+    public ArrayList<Empleado> leerEmpleados() {
+        ArrayList<Empleado> tmp = null;
+        try {
+            ObjectInputStream fLectura = new ObjectInputStream(
+                    new FileInputStream(archivo));
+
+            tmp = (ArrayList<Empleado>) fLectura.readObject();
+            if (tmp == null) {
+                System.out.println("No hay nada");
+            } else {
+                System.out.println("Nombre" + tmp.size());
+            }
+            fLectura.close();
+
+        } catch (Exception ex) {
+            System.out.println("Error" + ex.toString());
+        }
         //Leer del disco duro
         //Sesube al array list tmp
         return tmp;
     }
 }
-
